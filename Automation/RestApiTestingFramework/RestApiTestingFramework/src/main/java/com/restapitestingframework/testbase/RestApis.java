51824042fd156ca4020513da17d81b7782b9d228
test <test@tester.com>
2019-01-27 00:07:28 +0530

    Initial Framework and One TestCase Test Class

---
+++ Automation/RestApiTestingFramework/RestApiTestingFramework/src/main/java/com/restapitestingframework/testbase/RestApis.java
@@ -0,0 +1,12 @@
+
+package com.restapitestingframework.testbase;
+
+public class RestApis {
+	
+	public static String baseUri="http://dummy.restapiexample.com";
+	
+	public static String createEmployee="/api/v1/create";
+	
+	
+
+}
